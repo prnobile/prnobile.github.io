be89206592debd779adebf14daca98219fe621d0
prnobile <prnobile@gmail.com>
2014-06-14 01:06:15 -0400

    g

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ $( document ).ready(function()
 										}
 								}, 1000);
 });
+
+function initializeCastApi() 
+{
+        var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
+        var sessionRequest = new chrome.cast.SessionRequest(applicationID);
+        var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+                sessionListener,
+                receiverListener);
+        chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
+};
